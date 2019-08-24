14e66a22ed2dc6ed14aefec813c082bf466dca9e
Simon McVittie <smcv@debian.org>
2019-08-24 15:37:29 +0100

    tests: Show the actual output from tests if VERBOSE is set
    Writing tests' output to a log file makes them difficult to debug when
    the test might be running on an autobuilder or CI system where only
    stdout/stderr are recorded. This is particularly troublesome if a
    failure is only reproducible on a particular autobuilder.
    
    Recent Automake versions have the convention that detailed output from
    failing tests is written to stdout/stderr, not just to log files, when
    the VERBOSE environment variable is set; borrow that convention as a
    trigger for producing detailed test output.
    
    Signed-off-by: Simon McVittie <smcv@debian.org>
    Forwarded: https://gitlab.gnome.org/GNOME/cogl/merge_requests/14
    
    Gbp-Pq: Name tests-Show-the-actual-output-from-tests-if-VERBOSE-is-set.patch

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o pipefail
+
 if test -z "$G_DEBUG"; then
     G_DEBUG=fatal-warnings
 else
@@ -64,7 +66,12 @@ get_status()
 
 run_test()
 {
-  $($TEST_BINARY $1 &> "$LOG")
+  if [ -n "${VERBOSE-}" ]; then
+    echo "running $TEST_BINARY $1:"
+    $TEST_BINARY $1 2>&1 | tee "$LOG"
+  else
+    $($TEST_BINARY $1 &> "$LOG")
+  fi
   TMP=$?
   var_name=$2_result
   eval $var_name=$TMP
